b96d361cf614a0cdb2535360a5d49b025e3fdd06
mickeypugh <mickey.pugh@wayland.wbu.edu>
2020-09-30 16:24:13 -0500

    Initial commit

---
+++ getCityState.js
@@ -0,0 +1,59 @@
+// This is a request document using Ajax
+// function getPlace
+// parameter: zip code
+// action: create the XMLHttpRequest object, register the
+// handler for onreadystatechange, prepare to send
+
+// 			the request (with open), and send the request,
+// 			along with the zip code, to the server
+function getPlace(zip) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = receivePlace;
+    xhr.open("GET", "getCityState.php?zip=" + zip, true);
+    xhr.send(null);
+}
+function () {
+    if (xhr.readyState == 4 && status == 200) {
+        var result = xhr.responseText;
+        var place = result.split(',');
+        if (document.getElementById("city").value =="")
+            document.getElementById("city").value = place[0];
+
+        if (document.getElementById("state").value =="")
+            document.getElementById("state").value = place[1];
+    }
+}
+// getCityState.js
+// Ajax JavaScript code for the popcornA.html document
+
+/********************************************************/
+// function getPlace /
+// parameter: zip code
+// action: create the XMLHttpRequest object, register the
+// handler for onreadystatechange, prepare to send
+// the request (with open), and send the request,
+
+// along with the zip code, to the server
+// includes: the anonymous handler for onreadystatechange,
+// which is the receiver function, which gets the
+// response text, splits it into city and state,
+// and puts them into the document
+
+function getPlace(zip) {
+    var xhr = new XMLHttpRequest();
+
+// Register the embedded receiver function as the handler
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.responseText;
+            var place = result.split(',');
+            if (document.getElementById("city").value =="")
+                document.getElementById("city").value = place[0];
+            if (document.getElementById("state").value =="")
+                document.getElementById("state").value = place[1];
+        }
+    }
+    xhr.open("GET", "getCityState.php?zip=" + zip);
+    xhr.send(null);
+}
